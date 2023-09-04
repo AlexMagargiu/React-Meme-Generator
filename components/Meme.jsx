a428d41957eaeb735f5575d7a455b234d7c5a77b
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-09-04 16:03:37 +0300

    Make random image appear when pressing the button

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
+import memesData from "../memesData"
+
 export default function Meme(){
+    const [memeImage, setMemeImage] = useState("");
+
+    function getRandomMeme(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top text"></input>
                 <input type="text" className="form--input" placeholder="Bottom text"></input>
-                <button className="form--submit">Get a new meme image 🖼</button>
-            </form>
+                <button className="form--submit" onClick={getRandomMeme}>Get a new meme image 🖼</button>
+                <img src={memeImage} className="meme--image" />
+            </div>
         </main>
     )
 }
